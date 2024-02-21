139a09c69f3482f31adbaef0977cbaec9a64a51d
hiroshi nagao <hi_1205@hotmail.com>
2024-02-21 15:44:21 +0900

    home page css modify

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,7 +1,6 @@
 class Booking < ApplicationRecord
   belongs_to :user
   belongs_to :samurai
-
   validates :start_date, presence: true
   validates :end_date, presence: true
   enum status: { pending: "pending", approved: "approved", rejected: "rejected"}, _default: :pending
